548563318180ca1cb4335acba422eca28ba02a64
Eric Buzato Venarusso <36274852+ericvenarusso@users.noreply.github.com>
2021-08-30 20:44:24 -0300

    Update EventController.java

--- src/main/java/fiap/scj/restapi/apidemo/controllers/EventController.java
+++ src/main/java/fiap/scj/restapi/apidemo/controllers/EventController.java
@@ -29,7 +29,7 @@ public class EventController {
     @GetMapping("/test")
     public String test() {
         // GET http://localhost:8080/event/test
-        return "Teste efetuado com sucesso! Powered By Heroku";
+        return "<marquee>Teste efetuado com sucesso! Olá Eric</marquee>";
     }
 
     // Retrieve All
